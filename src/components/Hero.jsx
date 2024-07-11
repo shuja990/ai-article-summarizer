e73a7c7c31fda383c1ad5e822bc593bff93c35a8
Shuja Ali <shujaali1234@gmail.com>
2024-07-11 23:47:21 +0500

    Article sumarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import  logo  from "../assets/logo-intelli.png";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='sumz_logo' className='object-contain' width={250}/>
+
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>IntelliSummarize</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
